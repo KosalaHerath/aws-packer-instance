178b5ad8823ab74b70cb03a2687612b6fb6bf086
Your Name <sananthanak@gmail.com>
2018-12-18 12:58:46 +0530

    Update

--- apim/provision_db_apim.sh
+++ apim/provision_db_apim.sh
@@ -4,31 +4,37 @@
 echo "Running DB scripts for WSO2-APIM..."
 
 #Define parameter values for Database Engine and Version
+
 DB_ENGINE='CF_DBMS'
 DB_ENGINE_VERSION='CF_DBMS_VERSION'
 WSO2_PRODUCT_VERSION='CF_PRODUCT_VERSION'
 
 #Run database scripts for given database engine and product version
+
 if [[[ $DB_ENGINE = "postgres" ]] && [[ $WSO2_PRODUCT_VERSION = "2.1.0" ]]]; then
     echo "Postgres DB Engine Selected! Running WSO2-APIM 2.1.0 DB Scripts for Postgres..."
     export PGPASSWORD=CF_DB_PASSWORD
     psql -U CF_DB_USERNAME -h CF_DB_HOST -p CF_DB_PORT -d postgres -f /home/ubuntu/apim/apim210/apim_postgres.sql
+
 elif [[[ $DB_ENGINE = "postgres" ]] && [[$WSO2_PRODUCT_VERSION = "2.6.0" ]]]; then
     echo "Postgres DB Engine Selected! Running WSO2-APIM 2.6.0 DB Scripts for Postgres..."
     export PGPASSWORD=CF_DB_PASSWORD
     psql -U CF_DB_USERNAME -h CF_DB_HOST -p CF_DB_PORT -d postgres -f /home/ubuntu/apim/apim260/apim_postgres.sql
+
 elif [[[ $DB_ENGINE = "mysql" ]] && [[ $WSO2_PRODUCT_VERSION = "2.1.0" ]]]; then
     echo "MySQL DB Engine Selected! Running WSO2-APIM 2.1.0 DB Scripts for MySQL..."
     if [[ $DB_ENGINE_VERSION = "5.7" ]]; then
         mysql -u CF_DB_USERNAME -pCF_DB_PASSWORD -h CF_DB_HOST -P CF_DB_PORT < /home/ubuntu/apim/apim210/apim_mysql5.7.sql
     else
         mysql -u CF_DB_USERNAME -pCF_DB_PASSWORD -h CF_DB_HOST -P CF_DB_PORT < /home/ubuntu/apim/apim210/apim_mysql.sql
+
 elif [[[ $DB_ENGINE = "mysql" ]] && [[ $WSO2_PRODUCT_VERSION = "2.6.0" ]]]; then
     echo "MySQL DB Engine Selected! Running WSO2-APIM 2.6.0 DB Scripts for MySQL..."
     if [[ $DB_ENGINE_VERSION = "5.7" ]]; then
         mysql -u CF_DB_USERNAME -pCF_DB_PASSWORD -h CF_DB_HOST -P CF_DB_PORT < /home/ubuntu/apim/apim260/apim_mysql5.7.sql
     else
         mysql -u CF_DB_USERNAME -pCF_DB_PASSWORD -h CF_DB_HOST -P CF_DB_PORT < /home/ubuntu/apim/apim260/apim_mysql.sql
+
 elif [[[ $DB_ENGINE =~ 'oracle-se' ]] && [[ $WSO2_PRODUCT_VERSION = "2.1.0" ]]]; then
     echo "Oracle DB Engine Selected! Running WSO2-APIM 2.1.0 DB Scripts for Oracle..."
     # Create users to the required DB
@@ -43,6 +49,7 @@ elif [[[ $DB_ENGINE =~ 'oracle-se' ]] && [[ $WSO2_PRODUCT_VERSION = "2.1.0" ]]];
     echo exit | sqlplus64 WSO2AM_COMMON_DB/CF_DB_PASSWORD@//CF_DB_HOST:CF_DB_PORT/WSO2AMDB @/home/ubuntu/apim/apim210/apim_oracle_common_db.sql
     echo exit | sqlplus64 WSO2AM_APIMGT_DB/CF_DB_PASSWORD@//CF_DB_HOST:CF_DB_PORT/WSO2AMDB @/home/ubuntu/apim/apim210/apim_oracle_apimgt_db.sql
     mysql -u CF_DB_USERNAME -pCF_DB_PASSWORD -h CF_DB_HOST -P CF_DB_PORT < /home/ubuntu/apim/apim260/apim_mysql.sql
+
 elif [[[ $DB_ENGINE =~ 'oracle-se' ]] && [[ $WSO2_PRODUCT_VERSION = "2.6.0" ]]]; then
     echo "Oracle DB Engine Selected! Running WSO2-APIM 2.6.0 DB Scripts for Oracle..."
     # Create users to the required DB
@@ -56,10 +63,13 @@ elif [[[ $DB_ENGINE =~ 'oracle-se' ]] && [[ $WSO2_PRODUCT_VERSION = "2.6.0" ]]];
     echo exit | sqlplus64 CF_DB_USERNAME/CF_DB_PASSWORD@//CF_DB_HOST:CF_DB_PORT/WSO2AMDB @/home/ubuntu/apim/apim260/apim_oracle_user.sql
     echo exit | sqlplus64 WSO2AM_COMMON_DB/CF_DB_PASSWORD@//CF_DB_HOST:CF_DB_PORT/WSO2AMDB @/home/ubuntu/apim/apim260/apim_oracle_common_db.sql
     echo exit | sqlplus64 WSO2AM_APIMGT_DB/CF_DB_PASSWORD@//CF_DB_HOST:CF_DB_PORT/WSO2AMDB @/home/ubuntu/apim/apim260/apim_oracle_apimgt_db.sql
+
 elif [[[ $DB_ENGINE =~ 'sqlserver-se' ]] && [[ $WSO2_PRODUCT_VERSION = "2.1.0" ]]]; then
     echo "SQL Server DB Engine Selected! Running WSO2-APIM 2.1.0 DB Scripts for SQL Server..."
     sqlcmd -S CF_DB_HOST -U CF_DB_USERNAME -P CF_DB_PASSWORD -i /home/ubuntu/apim/apim210/apim_sql_server.sql
+
 elif [[[ $DB_ENGINE =~ 'sqlserver-se' ]] && [[ $WSO2_PRODUCT_VERSION = "2.6.0" ]]]; then
     echo "SQL Server DB Engine Selected! Running WSO2-APIM 2.6.0 DB Scripts for SQL Server..."
     sqlcmd -S CF_DB_HOST -U CF_DB_USERNAME -P CF_DB_PASSWORD -i /home/ubuntu/apim/apim260/apim_sql_server.sql
+    
 fi
